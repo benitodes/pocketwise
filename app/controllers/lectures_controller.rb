a39086d8bf44898e29663276e8cc4c2de42fafbb
jana meier <jana.meier@hotmail.de>
2019-08-13 21:47:06 +0800

    lectures part done

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -1,4 +1,8 @@
 class LecturesController < ApplicationController
   def show
+    @level = Level.find(params[:level_id])
+    @lecture = Lecture.find(params[:id])
+    @next_lecture = Lecture.where(level: @level).find_by(number: @lecture.number + 1)
+    @user = current_user
   end
 end
