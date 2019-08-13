b199393a00847c2fe629a64de32514fbf2080250
jana meier <jana.meier@hotmail.de>
2019-08-13 19:04:35 +0800

    category and courses flow done

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,4 +1,21 @@
 class CoursesController < ApplicationController
+  before_action :set_course, only: [:index]
+  before_action :course_params, only: [:index]
+
   def index
+    @category = Category.find(params[:category_id])
+    @courses = Course.all
+    # only display courses for specific category
+    @courses = @courses.select { |t| t.category == @category }
+  end
+
+  private
+
+  def set_course
+    # @course = Course.find(params[:id])
+  end
+
+  def course_params
+    # params.require(:course).permit(:category)
   end
 end
--- app/controllers/levels_controller.rb
+++ app/controllers/levels_controller.rb
@@ -1,7 +1,17 @@
 class LevelsController < ApplicationController
+  before_action :set_level, only: [:show]
+
   def index
+    @levels = Level.all
   end
 
   def show
+
+  end
+
+  private
+
+  def set_level
+    @level = Level.find(params[:id])
   end
 end
