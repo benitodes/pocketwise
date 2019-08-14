8978231ea9b7ba2c0125d86c552f10f59db14289
jana meier <jana.meier@hotmail.de>
2019-08-14 22:50:17 +0800

    first modal complete

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -5,5 +5,7 @@ class LecturesController < ApplicationController
     @next_lecture = Lecture.where(level: @level).find_by(number: @lecture.number + 1)
     # find user to get back to user dashboard
     @user = current_user
+    # find question so modal can route to first question of current level
+    @question = Question.where(level_id: params[:id]).find_by(number: 1)
   end
 end
