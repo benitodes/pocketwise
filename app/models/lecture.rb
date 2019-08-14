a47cb10bbac849367576a743e7123a13cd272647
jana meier <jana.meier@hotmail.de>
2019-08-14 11:10:13 +0800

    validations added

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -1,3 +1,4 @@
 class Lecture < ApplicationRecord
   belongs_to :level
+  validates :title, :description, :complete, presence: true
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,5 @@
 class Question < ApplicationRecord
   belongs_to :level
   has_many :answers, dependent: :destroy
+  validates :question_content, :complete, :hint, presence: true
 end
