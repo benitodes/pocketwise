256bcb59b145d3ef055f6aa8f3b0175ab337c27d
Samba Diaw <sambachimerediaw@gmail.com>
2019-08-16 17:16:33 +0800

    merge master

--- app/models/completed_level.rb
+++ app/models/completed_level.rb
@@ -7,13 +7,19 @@ class CompletedLevel < ApplicationRecord
     user = self.user_course.kid
     @wallet = user.kid_wallet
     @goal = @wallet.goals.where(complete: false).first
+    @next_goal = @wallet.goals.where(complete: false)[1]
     if @goal.nil?
       @wallet.payout_amount = @wallet.total_allowance
     else
       @wallet.payout_amount = @wallet.total_allowance - @goal.goal_allowance
       @goal.goal_current_saving += @goal.goal_allowance
       if @goal.goal_current_saving >= @goal.goal_price
-        @goal.completed == true
+        @goal.completed = true
+        @exceeded_amount = @goal.goal_current_saving - @goal.goal_price
+        if @next_goal
+          @next_goal.goal_current_saving = @exceeded_amount if @exceeded_amount > 0
+          @next_goal.save
+        end
       end
       @goal.save
     end
