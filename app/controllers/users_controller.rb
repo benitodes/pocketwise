085b56ce4a266f222ebb7a233f1f5c45aca00411
jana meier <jana.meier@hotmail.de>
2019-08-19 12:24:07 +0800

    progress circle for the learning of kid in parent dashboard done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,38 +4,17 @@ class UsersController < ApplicationController
   def dashboard
     @user = User.find(params[:id])
     authorize @user
+    @done_percentage = 0
     if @user.parent?
       @wallets = @user.parent_wallets
+      progress_percentage(@wallets.first.kid_id)
     else
       @wallet = @user.kid_wallet
       @goals = Goal.where(wallet_id: @wallet)
       @user_course = UserCourse.where(kid: current_user).first
       unless @user_course.nil?
         @course = Course.where(id: @user_course.course_id)
-        # find total amount of questions in course for circle
-        @levels = Level.where(course_id: @course)
-        @total_questions_per_course = 0
-        @levels.each do |level|
-          level.number_of_questions = Question.where(level_id: level).length
-          # save in db
-          level.save
-          # add up to total
-          @total_questions_per_course += level.number_of_questions
-        end
-        # find total amount of questions answered so far by user
-        # find last completed level in course
-        # for completed level calculate amount of questions and add them up
-        @completed_levels = CompletedLevel.where(user_course_id: @user_course)
-        @total_questions_completed = 0
-        @completed_levels.each do |completed_level|
-          @number_questions_in_level = Question.where(level_id: completed_level.level_id).length
-          @total_questions_completed += @number_questions_in_level
-        end
-        # add last_question number from user course table - 1
-        @total_questions_completed += @user_course.last_question - 1
-        # calculate percentage done
-        @done_percentage = @total_questions_completed.to_f / @total_questions_per_course.to_f * 100
-
+        progress_percentage(current_user)
         # check if course is complete. if level nr > number of levels in current course then complete
         if @user_course.last_level > Level.where(course_id: @course).length
           @user_course.complete = true
@@ -43,21 +22,6 @@ class UsersController < ApplicationController
         end
       end
     end
-
-
-  #   if @user.parent?
-  #     @wallets.each do |wallet|
-  #       @kid = wallet.kid
-  #     end
-  #       @enrolled = @kid.user_courses.where(complete: false)
-  #       @enrolled.each do |user_course|
-  #         @course_id = user_course.course_id
-  #         @course = Course.find(@course_id)
-  #         @last_level = user_course.last_level
-  #         @last_question = user_course.last_question
-  #         @last_lecture = user_course.last_lecture
-  #       end
-  #   end
   end
 
   def new
@@ -84,6 +48,8 @@ class UsersController < ApplicationController
     redirect_to dashboard_user_path(current_user)
   end
 
+  private
+
   def set_user
     @user = User.find(params[:id])
   end
@@ -92,4 +58,33 @@ class UsersController < ApplicationController
     params.require(:user).permit(:first_name, :last_name, :username, :avatar, :gender, :email, :password, :age, :parent)
   end
 
+  def progress_percentage(kid_id)
+    @user_course = UserCourse.where(kid: kid_id).first
+      unless @user_course.nil?
+        @course = Course.where(id: @user_course.course_id)
+        # find total amount of questions in course for circle
+        @levels = Level.where(course_id: @course)
+        @total_questions_per_course = 0
+        @levels.each do |level|
+          level.number_of_questions = Question.where(level_id: level).length
+          # save in db
+          level.save
+          # add up to total
+          @total_questions_per_course += level.number_of_questions
+        end
+        # find total amount of questions answered so far by user
+        # find last completed level in course
+        # for completed level calculate amount of questions and add them up
+        @completed_levels = CompletedLevel.where(user_course_id: @user_course)
+        @total_questions_completed = 0
+        @completed_levels.each do |completed_level|
+          @number_questions_in_level = Question.where(level_id: completed_level.level_id).length
+          @total_questions_completed += @number_questions_in_level
+        end
+        # add last_question number from user course table - 1
+        @total_questions_completed += @user_course.last_question - 1
+        # calculate percentage done
+        @done_percentage = @total_questions_completed.to_f / @total_questions_per_course.to_f * 100
+      end
+  end
 end
